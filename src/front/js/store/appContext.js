0ef9f4acba14be89a7ba98126a72285f34815ffb
Evelyn <130219194+EveyZeram@users.noreply.github.com>
2024-01-26 20:53:51 +0000

    Evey Maps Branch

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -24,7 +24,7 @@ const injectContext = (PassedComponent) => {
 		);
 
 		useEffect(() => {
-			// Get Pharmacies
+			// Añadir 
 			state.actions.getPharmacies();
 			/* EDIT THIS!
 			   This function is the equivalent to "window.onLoad", it only runs once on the entire application lifetime
